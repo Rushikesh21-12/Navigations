525b55dba865e109cfb3ae311df104d238c48569
Rushi <rushisolanki6@gmail.com>
2022-03-08 17:06:25 +0530

    parent 07d69b25b136706b0b139b4e776ac32212d256a8 author Rushi <rushisolanki6@gmail.com> 1646739385 +0530 committer Rushi <rushisolanki6@gmail.com> 1647406427 +0530

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import AppStack from './AppStack';
 import AppBottomTab from './AppBottomTab';
 import AppTabStack from './AppTabStack';
 import AppStackTab from './AppStackTab';
+ok
 import AppMB from './AppMB';
 import AppMT from './AppMT';
 import {name as appName} from './app.json';
@@ -20,7 +21,7 @@ AppRegistry.registerComponent(appName, () => AllTabs);
 //Changes made by another dev
 //Changes made by another dev
 //Changes made by another dev
-//Changes made by another dev
+//Changes made by another devsaf
 //Changes made by another dev
 //Changes made by another dev
 //Changes made by another dev
