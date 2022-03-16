e4124c61410829500c015eb1cabec2e0aa799ec0
Rushi <rushisolanki6@gmail.com>
2022-03-16 09:33:53 +0530

    Merge commits

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import AppStack from './AppStack';
 import AppBottomTab from './AppBottomTab';
 import AppTabStack from './AppTabStack';
 import AppStackTab from './AppStackTab';
+ok
 import AppMB from './AppMB';
 import AppMT from './AppMT';
 import {name as appName} from './app.json';
@@ -20,9 +21,11 @@ AppRegistry.registerComponent(appName, () => AllTabs);
 //Changes made by another dev
 //Changes made by another dev
 //Changes made by another dev
-//Changes made by another dev
+//Changes made by another devsaf
 //Changes made by another dev
 //Changes made by another dev
 //Changes made by another dev
 //Changes to check
 // changes in dev-Rushikesh branch
+// bbbb branch
+
